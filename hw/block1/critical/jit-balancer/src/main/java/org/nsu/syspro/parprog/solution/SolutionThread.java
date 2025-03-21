671729fc48855a4bb3e41a5cc331e6992c3ac91c
malaartyom <malarenkoart@bk.ru>
2025-03-21 12:16:34 +0700

    Add Medium part of critical-1 task.
    Now I'am using ReadWriteLock instead of syncronization on Object lock
    Level two compilation of methods happends in other thread. For this purpose I use
    ExecutorService.

--- hw/block1/critical/jit-balancer/src/main/java/org/nsu/syspro/parprog/solution/SolutionThread.java
+++ hw/block1/critical/jit-balancer/src/main/java/org/nsu/syspro/parprog/solution/SolutionThread.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class SolutionThread extends UserThread {
 
@@ -29,15 +31,14 @@ public class SolutionThread extends UserThread {
 
     /**
      * A thread-safe pool of threads for compiling methods asynchronously.
-     *
-     * BTW it is not yet used :)
+     * <p>
      */
     private static final ExecutorService executor = Executors.newCachedThreadPool();
 
     /**
-     * An object used for synchronization to ensure thread safety when working with shared resources.
+     * A ReadWriteLock to work with shared resources.
      */
-    private final static Object lock = new Object();
+    private final static ReadWriteLock lock = new ReentrantReadWriteLock();
 
     private static final int L1 = 20;
     private static final int L2 = 900;
@@ -47,19 +48,20 @@ public class SolutionThread extends UserThread {
      * Increments the usage count for the specified {@link MethodID}.
      * <p>
      * This method updates the {@link #usages} HashMap in a thread-safe manner
-     * using synchronization on lock. If the method ID is not already in the map,
+     * using {@link ReadWriteLock} {@link #lock}. If the method ID is not already in the map,
      * it is initialized with a count of zero before being incremented.
      * </p>
      *
      * @param id the {@link MethodID} whose usage count should be incremented
      */
     private void incUsages(MethodID id) {
-        synchronized (lock) {
-            if (!usages.containsKey(id.id())) {
-                usages.put(id.id(), 0);
-            }
-            usages.put(id.id(), usages.get(id.id()) + 1);
+        lock.writeLock().lock();
+        try {
+            usages.put(id.id(), usages.getOrDefault(id.id(), 0) + 1);
+        } finally {
+            lock.writeLock().unlock();
         }
+
     }
 
     public SolutionThread(int compilationThreadBound, ExecutionEngine exec, CompilationEngine compiler, Runnable r) {
@@ -77,61 +79,69 @@ public class SolutionThread extends UserThread {
     /**
      * Checks if method should be compiled and what level of compilation should be used
      * <p>
-     *     This method uses information in {@link #usages} map to check if specified method
-     *     needs compilation. If usages of method with {@link #id} more than some constant L1
-     *     then method checks if higher level of compilation is needed. If so compiler
-     *     compiles method with given {@link #id} by l2 compiler. Otherwise, checks if
-     *     method is already compiled. If not, method with given {@link #id} is compiled by l1
-     *     compiler
+     * This method uses information in {@link #usages} map to check if specified method
+     * needs compilation. If usages of method with {@link #id} more than some constant L1
+     * then method checks if higher level of compilation is needed. If so compiler
+     * compiles method with given {@link #id} by l2 compiler. Otherwise, checks if
+     * method is already compiled. If not, method with given {@link #id} is compiled by l1
+     * compiler
      *
      * </p>
-     * <p>
-     *   This method is thread-safe as it synchronizes on {@link #lock} to ensure
-     *   atomic updates to the {@link #usages} map and compilation process.
-     * </p>
+     *
      * @param id - the {@link MethodID} which we want to check
      */
     private void check(MethodID id) {
-        synchronized (lock) {
+        lock.writeLock().lock();
+        try {
             if (usages.get(id.id()) >= L1) {
                 if (usages.get(id.id()) >= L2 && !compiledL2.contains(id.id())) {
                     compiledL2.add(id.id());
-                    methods.put(id.id(), compiler.compile_l2(id));
+                    executor.submit(() ->
+                        methods.put(id.id(), compiler.compile_l2(id)));
                 } else if (!methods.containsKey(id.id())) {
                     methods.put(id.id(), compiler.compile_l1(id));
                 }
-            }
-        }
 
+            }
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     /**
      * Returns already compiled method using method id
      * and {@link #methods} map.
+     * <p>
+     * This method is thread-safe as it uses {@link ReadWriteLock} {@link #lock}
      *
-     * This method is thread-safe as it synchronizes on lock
      * @param id
      * @return {@link CompiledMethod}
      */
     private CompiledMethod getCompiled(MethodID id) {
-        synchronized (lock) {
+        lock.readLock().lock();
+        try {
             return methods.get(id.id());
+        } finally {
+            lock.readLock().unlock();
         }
     }
 
     /**
      * This method checks whether specified method is compiled.
      * For this purpose {@link #methods} map is used.
-     *  <p>
-     *      Method is thread-safe and uses synchronization on lock.
-     *  </p>
+     * <p>
+     * Method is thread-safe and uses {@link ReadWriteLock} {@link #lock}.
+     * </p>
      *
      * @param id
      * @return {@code true} if method is compiled, {@code false} if not.
      */
-    synchronized private boolean isCompiled(MethodID id) {
-        synchronized (lock) {
+    private boolean isCompiled(MethodID id) {
+        lock.readLock().lock();
+        try {
             return methods.containsKey(id.id());
+        } finally {
+            lock.readLock().unlock();
         }
     }
 
